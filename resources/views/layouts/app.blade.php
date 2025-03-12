e930dcdcf7b4627f18258b616c710701d987ae50
juma <nahshonjumaz@gmail.com>
2025-03-12 21:25:30 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,5 +4,5 @@
 @yield('content')
 
  <footer class="tm-footer text-center">
-    <p>Copyright &copy; 2024 Simple Safari | Design: <a rel="nofollow" href="https://templatemo.com">Jumaz</a></p>
+    <p>Copyright &copy; 2024 Simple Safari | Design: <a rel="nofollow" href="https://templatemo.com">nJuma</a></p>
 </footer>
