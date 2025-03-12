00c2d5d46704e593b21c1ef7da853e7a0da12138
juma <nahshonjumaz@gmail.com>
2025-03-12 21:24:51 +0300

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,9 +5,13 @@
 	<meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
     <link rel="shortcut icon" href='img/icon.png' type="image/x-icon">
+
     <title>Simple Safari</title>
+
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" />
+
     <link href="css/all.min.css" rel="stylesheet" />
 	<link href="css/templatemo-style.css" rel="stylesheet" />
 </head>
