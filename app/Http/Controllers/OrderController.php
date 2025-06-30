2db894287985e5f9591008d3414e462256cf7276
juma <nahshonjumaz@gmail.com>
2025-06-30 19:03:58 +0300

    get all pizzas

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     public function index(){
-        // $pizzas=order::all();
+        $pizzas=order::all();
         // $pizzas=order::orderBy('name','desc')->get();
-         $pizzas=order::where('type','gino')->get();
+         // $pizzas=order::where('type','gino')->get();
         // $pizzas=order::latest()->get();
         return view('pizza.order',[
             'pizzas'=>$pizzas
